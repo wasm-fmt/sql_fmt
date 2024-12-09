324d5707c34b1aa3bab2078f2cf742f614d48e8d
magic-akari <akari.ccino@gmail.com>
2024-12-09 11:36:06 +0800

    chore(deps): update dependencies

--- extra/sql_fmt_node.js
+++ extra/sql_fmt_node.js
@@ -3,8 +3,8 @@ import initAsync from "./sql_fmt.js";
 
 const wasm = new URL("./sql_fmt_bg.wasm", import.meta.url);
 
-export default function __wbg_init(init = fs.readFile(wasm)) {
-    return initAsync(init);
+export default function __wbg_init(init = { module_or_path: fs.readFile(wasm) }) {
+	return initAsync(init);
 }
 
 export * from "./sql_fmt.js";
